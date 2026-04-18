8103e399148de62a058ce15c199e66d9f4a8064f
spartanabzz467-ui <spartanabzz467@gmail.com>
2026-04-18 12:36:26 +0300

    done

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,39 @@
-const {
-  combineUsers
-  } = require('../index');
-  require('datejs');
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
 
-  describe('combineUsers function return', () => {
-    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
-    
-    
-    test('should be an object', () => {
-      expect(typeof combine).toBe('object');
-    });
-    test('should have the properties users and merge_date', () => {
-      expect(combine).toHaveProperty('users');
-      expect(combine).toHaveProperty('merge_date');
-    });
+  posts.forEach(post => {
+
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
   });
-  describe('combineUsers function', () => {
-    const combine = combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
-    const combine2 = combineUsers(["test1"],["test2"])
-    const combine3 = combineUsers([])
-    
-    test('should merge all given arrays', () => {
-      expect(combine.users).toStrictEqual(["Jim3","Pam5","Dwight77","Michael6","Eleanor22","Chidi202","Jack_jack","Julia_Oreo", "Bill_bore"]);
-      expect(combine2.users).toStrictEqual(["test1","test2"])
-      expect(combine3.users).toStrictEqual([])
-    });
+}
 
-    test('should contain todays date', () => {
-      expect(combine.merge_date).toBe(Date.today().toString("M/d/yyyy"))
-      expect(combine2.merge_date).toBe(Date.today().toString("M/d/yyyy"))
-    });
-    
-  })
+async function fetchPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+
+    const data = await response.json();
+
+    console.log(data); 
+
+    displayPosts(data);
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+fetchPosts();
   
